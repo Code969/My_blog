22e8ff96fd7f20a95ae8b694a05a7835477dbdf6
Code969 <87217364+Code969@users.noreply.github.com>
2021-08-14 19:20:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SECRET_KEY'] = os.environ.get("secret_key")
 ckeditor = CKEditor(app)
 Bootstrap(app)
